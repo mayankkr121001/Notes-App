aede25830d2089fa0e9dd0c54a3429e18b82150a
mayankkr121001 <mayanksahu121001@gmail.com>
2024-08-27 18:59:03 +0530

    Preparation for deploying

--- notes-here/src/interceptors/axios.js
+++ notes-here/src/interceptors/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.VITE_BACKEND_API_BASE_URL,
     withCredentials: true
 })
 
